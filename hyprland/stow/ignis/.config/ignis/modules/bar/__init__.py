b4f2a3dfc3c3d090b9b0bb2b42d367bf24f03b29
TTluke <pollak.lukas06@gmail.com>
2024-12-04 20:59:07 +0100

    5. commit

--- hyprland/stow/ignis/.config/ignis/modules/bar/__init__.py
+++ hyprland/stow/ignis/.config/ignis/modules/bar/__init__.py
@@ -18,7 +18,6 @@ def bar(monitor: int) -> Widget.Window:
         child=Widget.CenterBox(
             css_classes=["bar-widget"],
             start_widget=Widget.Box(child=[workspaces()]),
-            center_widget=Widget.Box(child=[pinned_apps()]),
             end_widget=Widget.Box(child=[tray(), kb_layout(), battery_widget(), clock(monitor)]),
         ),
         css_classes=["unset"],
