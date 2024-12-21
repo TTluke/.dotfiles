80e2a43ed60c6c0dbe9236c0ad3b0c15544c9553
TTluke <pollak.lukas06@gmail.com>
2024-12-21 19:16:36 +0100

    7. commit

--- hyprland/stow/fish/.config/fish/config.fish
+++ hyprland/stow/fish/.config/fish/config.fish
@@ -1,12 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-fish_add_path /home/link/.spicetify
-fish_add_path /home/link/.bin
-
-if uwsm check may-start
-    exec uwsm start hyprland.desktop
-end
 
 alias ls="eza -l --icons"
 
