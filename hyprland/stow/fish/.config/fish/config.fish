92e49058d91663fd4ddc40ac37961c6df24d23c5
TTluke <pollak.lukas06@gmail.com>
2024-12-26 09:53:59 +0100

    8. commit

--- hyprland/stow/fish/.config/fish/config.fish
+++ hyprland/stow/fish/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function sesh
+    /usr/bin/sesh connect (/usr/bin/sesh list | fzf)
+end
+
 alias ls="eza -l --icons"
 
 zoxide init --cmd cd fish | source
